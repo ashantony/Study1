96561c4d6dd7838d7027eba5193b63da864d8ff2
asha <asha.antony@inapp.com>
2018-03-11 22:07:09 +0530

    angular4-study:- file upload form created- functionality pending

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { ImageUploadFormComponent } from './image-upload-form/image-upload-form.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Image Gallery';
+  showForm = false;
+
+  popUpImageUploadForm(){
+  // alert("heloo");
+  this.showForm = true;
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+// import { Ng4FilesModule } from './ng4-files';
+
 
 
 import { AppComponent } from './app.component';
+import { ImageUploadFormComponent } from './image-upload-form/image-upload-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageUploadFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    // Ng4FilesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
