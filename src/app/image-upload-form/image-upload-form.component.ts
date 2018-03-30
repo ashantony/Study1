74548b3ae0a022d479c40c0bd34b4b991b07d5a5
akhilrs <me@akhil.rs>
2018-03-30 12:49:55 +0530

    Added  heroku service and fixed upload issue

--- src/app/image-upload-form/image-upload-form.component.ts
+++ src/app/image-upload-form/image-upload-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ImguploadserviceService } from 'app/imguploadservice.service';
+import {Component, OnInit} from '@angular/core';
+import {ImguploadserviceService} from 'app/imguploadservice.service';
+
 // import {NgForm} from '@angular/forms';
 
 @Component({
@@ -8,18 +9,20 @@ import { ImguploadserviceService } from 'app/imguploadservice.service';
   styleUrls: ['./image-upload-form.component.css']
 })
 export class ImageUploadFormComponent implements OnInit {
-     public base64textString:String="";
-   public caption: string;
-   public image: any;
-   result = [];
-   selctedFile: File = null;
-   // imgForm: ngForm;
-   constructor(private httpservice: ImguploadserviceService) {}
+  public base64textString: String = '';
+  public caption: string;
+  public image: any;
+  result = [];
+  selctedFile: File = null;
+
+  // imgForm: ngForm;
+  constructor(private httpservice: ImguploadserviceService) {
+  }
 
   ngOnInit() {
   }
-  onFileSelect(event){
-    console.log(event);
+
+  onFileSelect(event) {
     this.selctedFile = <File>event.target.files[0];
     // ....
     let reader = new FileReader();
@@ -41,39 +44,40 @@ export class ImageUploadFormComponent implements OnInit {
 //       var file = files[0];
 
     if (event.target.files && this.selctedFile) {
-        var reader = new FileReader();
+      reader = new FileReader();
 
-        reader.onload =this._handleReaderLoaded.bind(this);
+      reader.onload = this._handleReaderLoaded.bind(this);
 
-        reader.readAsBinaryString(this.selctedFile);
+      reader.readAsBinaryString(this.selctedFile);
     }
 
     // ................
   }
+
   // ..........
   _handleReaderLoaded(readerEvt) {
-var binaryString = readerEvt.target.result;
-      this.base64textString= btoa(binaryString);
-      console.log( this.base64textString);
-}
+    var binaryString = readerEvt.target.result;
+    this.base64textString = btoa(binaryString);
+  }
 
 
   // ...........
   sbumitted = false;
-onSubmit() {
-    alert("helloo");
-    debugger
-    const data = this.base64textString
+
+  onSubmit() {
+    // debugger;
+    const imageData = this.base64textString;
     //
     // fd.append('image', this.selctedFile, this.selctedFile.name);
-    this.httpservice.uploadImage(data)
+    const payload = {
+      'data': imageData,
+      'format': 'jpg',
+      'width': 500,
+      'height': 500
+    };
+    this.httpservice.uploadImage(payload)
+      .subscribe((res) => console.log(res));
 
-    // console.log(fd);
-    .subscribe(
-    (response) => {
-      console.log(response);
-    });
-
-  // this.submitted = true;
- }
+    // this.submitted = true;
+  }
 }
